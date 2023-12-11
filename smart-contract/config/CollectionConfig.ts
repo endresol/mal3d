09715b6a4e5b7ded28152ec2947ded4026ee101a
Endre Solem <esolem@gmail.com>
2023-12-11 10:32:23 +0100

    getting started

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -1,18 +1,15 @@
-import CollectionConfigInterface from '../lib/CollectionConfigInterface';
-import * as Networks from '../lib/Networks';
-import * as Marketplaces from '../lib/Marketplaces';
-import whitelistAddresses from './whitelist.json';
+import CollectionConfigInterface from "../lib/CollectionConfigInterface";
+import * as Networks from "../lib/Networks";
+import * as Marketplaces from "../lib/Marketplaces";
+import whitelistAddresses from "./whitelist.json";
 
 const CollectionConfig: CollectionConfigInterface = {
   testnet: Networks.ethereumTestnet,
   mainnet: Networks.ethereumMainnet,
-  // The contract name can be updated using the following command:
-  // yarn rename-contract NEW_CONTRACT_NAME
-  // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: "MoonApeLab3D",
+  tokenName: "Moon Ape Lab 3D",
+  tokenSymbol: "MAL3D",
+  hiddenMetadataUri: "ipfs://__CID__/hidden.json",
   maxSupply: 10000,
   whitelistSale: {
     price: 0.05,
@@ -27,7 +24,7 @@ const CollectionConfig: CollectionConfigInterface = {
     maxMintAmountPerTx: 5,
   },
   contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  marketplaceIdentifier: "moon-ape-lab-3d",
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
