748d8341c3bd4636ca6100acad2870bd38c5680d
Endre Solem <esolem@gmail.com>
2024-03-14 18:46:16 +0100

    fixed sc call

--- mint3d-next/hooks/useIs2DMinted.ts
+++ mint3d-next/hooks/useIs2DMinted.ts
@@ -18,7 +18,7 @@ export const useIs2DMinted = (tokenid: BigNumber) => {
 
     const getContractStatus = async () => {
       try {
-        const _mintStatus = await contract.is2dMinted(tokenid);
+        const _mintStatus = await contract.ownerOf(tokenid);
         setMintStatus(_mintStatus);
       } catch (err) {
         console.error("getContractStatus", err);
